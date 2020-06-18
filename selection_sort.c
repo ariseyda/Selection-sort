64b79d298224972d7c386ea82e81bc6da0663943
Şeyda Arı <ariseydaari@gmail.com>
2020-06-18 17:01:34 +0300

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -3,6 +3,11 @@
 #define MAX 50
 
 //Selection Sort
+//The smallest number in the array is detected
+//Sorts from small to large
+//The numbers to the right of the min value are checked.
+//If a number less than min is found, it is replaced with the min value.
+
 
 int main(){
 	
@@ -22,6 +27,10 @@ int main(){
 	for(j=0;j<size;j++){
 		
 		minimum=array[j];
+		//I compare the values on the right side of j with j.
+		//And if I find a value less than j, I swapp it with j.
+		//The number of comparisons made with each step decreases.
+		//Because the left side is sorted first and you continue to line up the numbers on the right side.
 		
 		for(i=j+1;i<size;i++){
 			
